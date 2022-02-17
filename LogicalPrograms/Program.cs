02efe140e2cd873ba1ad525e91cd001fb6d4e933
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-17 19:30:46 +0530

    [Amit] Added logical programs

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,7 +11,7 @@ namespace Logical_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("Choose the program");
-            Console.WriteLine("\n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse Number \n 5. Coupon Numbers \n 6. Day of Week ");
+            Console.WriteLine("\n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse Number \n 5. Coupon Numbers \n 6. Day of Week  \n 7. Simulate Stop Watch \n 8. Temperature Conversion ");
             int programNum = Convert.ToInt32(Console.ReadLine());
             switch (programNum)
             {
@@ -33,7 +33,12 @@ namespace Logical_Programs
                 case 6:
                     DayOfWeek.FindDayOfWeek();
                     break;
-
+                case 7:
+                    SimulateStopWatch.GetElapsedTime();
+                    break;
+                case 8:
+                    TemperatureConversion.ConvertTemp();
+                    break;
                     Console.WriteLine("Please choose the correct ProgramNum");
                     break;
             }
---
+++ LogicalPrograms/SimulateStopWatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class SimulateStopWatch
+    {
+        public static void GetElapsedTime()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Enter any key to stop stopwatch");
+            Console.ReadLine();     
+            stopwatch.Stop();
+            Console.WriteLine("\n Elapsed time : " + stopwatch.Elapsed);
+        }
+    }
+}
